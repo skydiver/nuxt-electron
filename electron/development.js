a777039dc45cb488505d73a16ae494145ba154c2
Martín M <skydiver@users.noreply.github.com>
2019-05-24 23:45:32 -0300

    dev loader cleanup

--- electron/development.js
+++ electron/development.js
@@ -1,55 +1,36 @@
 /* eslint-disable no-console */
 const path = require('path');
-const http = require('http');
-const electron = require('electron');
+const { app, BrowserWindow } = require('electron');
+const {
+  default: installExtension,
+  VUEJS_DEVTOOLS
+} = require('electron-devtools-installer');
 
 const loader = () => {
   const _NUXT_URL_ = `http://localhost:3000`;
 
-  const config = {};
-  config.dev = false;
-
-  let win = null;
-  const app = electron.app;
+  let mainWindow = null;
 
   const newWin = () => {
-    win = new electron.BrowserWindow({
+    mainWindow = new BrowserWindow({
       icon: path.join(__dirname, 'static/icon.png')
     });
+    mainWindow.maximize();
+    mainWindow.on('closed', () => (mainWindow = null));
 
-    win.maximize();
-    win.on('closed', () => (win = null));
-    win.webContents.openDevTools();
-
-    // Install vue dev tool and open chrome dev tools
-    const {
-      default: installExtension,
-      VUEJS_DEVTOOLS
-    } = require('electron-devtools-installer');
     installExtension(VUEJS_DEVTOOLS.id)
       .then(name => {
-        console.log(`Added Extension:  ${name}`);
-        win.webContents.openDevTools();
+        mainWindow.webContents.openDevTools();
       })
       .catch(err => console.log('An error occurred: ', err));
-    // Wait for nuxt to build
-    const pollServer = () => {
-      http
-        .get(_NUXT_URL_, res => {
-          if (res.statusCode === 200) {
-            win.loadURL(_NUXT_URL_);
-          } else {
-            setTimeout(pollServer, 300);
-          }
-        })
-        .on('error', pollServer);
-    };
-    pollServer();
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.loadURL(_NUXT_URL_);
   };
 
   app.on('ready', newWin);
   app.on('window-all-closed', () => app.quit());
-  app.on('activate', () => win === null && newWin());
+  app.on('activate', () => mainWindow === null && newWin());
 };
 
 module.exports = { loader };
